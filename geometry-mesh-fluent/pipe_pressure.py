d952235950aa1beedfbbd45e980b1c0f7522ac34
Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>
2023-12-26 17:28:26 +0000

    Mesh the pipe.

--- geometry-mesh-fluent/pipe_pressure.py
+++ geometry-mesh-fluent/pipe_pressure.py
@@ -6,8 +6,9 @@ from ansys.geometry.core.sketch import Sketch
 
 # draw pipe
 # plane = Plane(origin=Point3D([0,0,0]), direction_x=Vector3D([1,0,0]), direction_y=Vector3D([0,0,1]))
+# sketch = Sketch(plane)
 
-sketch = Sketch(plane)
+sketch = Sketch()
 
 (
     sketch.circle(Point2D([0, 0], UNITS.mm), Quantity(2, UNITS.cm), "input")
@@ -27,3 +28,44 @@ body = design.extrude_sketch(
 
 # Plot the body
 design.plot()
+
+# Download and save design
+cad_file = "D:\\test"
+design.download(cad_file, as_stream=False)
+
+
+# Import the geometry and mesh it
+import os
+import tempfile
+
+from ansys.meshing import prime
+from ansys.meshing.prime.graphics import Graphics
+
+prime_client = prime.launch_prime()
+model = prime_client.model
+mesh_util = prime.lucid.Mesh(model=model)
+
+# with prime.launch_prime() as session:
+#     model = session.model
+
+#     with prime.FileIO(model) as io:
+#         _ = io.import_cad(cad_file, params=prime.ImportCADParams(model))
+#     prime.lucid.Mesh(model=model)
+
+mesh_util.read(file_name=cad_file)
+mesh_util.create_zones_from_labels("inlet,outlet")
+
+# Surface meshing
+mesh_util.surface_mesh(min_size=5, max_size=20)
+
+# Volume meshing
+mesh_util.volume_mesh(
+    volume_fill_type=prime.VolumeFillType.POLY,
+    prism_surface_expression="* !inlet !outlet",
+    prism_layers=3,
+)
+
+# Display the mesh
+display = Graphics(model=model)
+display()
+
