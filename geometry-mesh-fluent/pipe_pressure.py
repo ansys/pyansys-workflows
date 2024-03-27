5ef92191583c0f13e192444ff58d05896290015b
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2024-03-27 12:50:17 +0100

    geometry

--- geometry-mesh-fluent/pipe_pressure.py
+++ geometry-mesh-fluent/pipe_pressure.py
@@ -1,20 +1,40 @@
-from pint import Quantity
+# Copyright (C) 2024 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-from ansys.geometry.core.math import Plane, Point2D, Point3D, Vector3D
-from ansys.geometry.core.misc import UNITS
-from ansys.geometry.core.sketch import Sketch
+# import ansys.fluent.core as pyfluent
+# from ansys.geometry.core import Modeler
+# from ansys.geometry.core.math import Plane, Point2D, Point3D, Vector3D
+# from ansys.geometry.core.misc import UNITS
+# from ansys.geometry.core.misc.measurements import Distance
+# from ansys.geometry.core.sketch import Sketch
+# from pint import Quantity
 
-from ansys.geometry.core.misc.measurements import Distance
-
-from ansys.geometry.core import Modeler
-
-import ansys.fluent.core as pyfluent
-
-
-print ("Hello World")
+# print("Hello World")
 
 # # draw pipe
-# plane = Plane(origin=Point3D([0,0,0]), direction_x=Vector3D([1,0,0]), direction_y=Vector3D([0,0,1]))
+# plane = Plane(origin=Point3D([0,0,0]),
+#               direction_x=Vector3D([1,0,0]),
+#               direction_y=Vector3D([0,0,1])
+#               )
 # sketch = Sketch(plane)
 
 # sketch = Sketch()
@@ -48,12 +68,16 @@ print ("Hello World")
 
 
 # sketch3 = Sketch()
-# plane = Plane(origin=Point3D([-4,0,0], UNITS.cm), direction_x=Vector3D([0,-1,0]), direction_y=Vector3D([0,0,-1]))
+# plane = Plane(origin=Point3D([-4,0,0], UNITS.cm),
+#               direction_x=Vector3D([0,-1,0]),
+#               direction_y=Vector3D([0,0,-1]))
 # sketch3 = Sketch(plane)
 # sketch3.box(Point2D([0,0]), Quantity(6, UNITS.cm), Quantity(6, UNITS.cm))
 
 # sketch4 = Sketch()
-# plane = Plane(origin=Point3D([-4,0,0], UNITS.cm), direction_x=Vector3D([0,-1,0]), direction_y=Vector3D([0,0,-1]))
+# plane = Plane(origin=Point3D([-4,0,0], UNITS.cm),
+#               direction_x=Vector3D([0,-1,0]),
+#               direction_y=Vector3D([0,0,-1]))
 # sketch4 = Sketch(plane)
 # sketch4.box(Point2D([0,0]), Quantity(3, UNITS.cm), Quantity(3, UNITS.cm))
 
@@ -170,7 +194,6 @@ print ("Hello World")
 # velocity_symmetry.surfaces_list = ["symmetry-xyplane"]
 # velocity_symmetry.scale.scale_f = 4
 # velocity_symmetry.style = "arrow"
-
 
 
 # # Display contour
--- geometry-mesh-fluent/pipe_pressure_debug.py
+++ geometry-mesh-fluent/pipe_pressure_debug.py
@@ -1,28 +1,55 @@
-from pint import Quantity
-
-from ansys.geometry.core.math import Plane, Point2D, Point3D, Vector3D
-from ansys.geometry.core.misc import UNITS
-from ansys.geometry.core.sketch import Sketch
-
-from ansys.geometry.core.misc.measurements import Distance
+# Copyright (C) 2024 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 from ansys.geometry.core import Modeler
+from ansys.geometry.core.math import Plane, Point2D, Point3D, Vector3D
+from ansys.geometry.core.misc import UNITS
+from ansys.geometry.core.misc.measurements import Distance
+from ansys.geometry.core.sketch import Sketch
+from pint import Quantity
 
 # Start by creating the Design
 modeler = Modeler()
 design = modeler.create_design("Pipe")
 
 sketch3 = Sketch()
-plane = Plane(origin=Point3D([-4,0,0], UNITS.cm), direction_x=Vector3D([0,-1,0]), direction_y=Vector3D([0,0,-1]))
+plane = Plane(
+    origin=Point3D([-4, 0, 0], UNITS.cm),
+    direction_x=Vector3D([0, -1, 0]),
+    direction_y=Vector3D([0, 0, -1]),
+)
 sketch3 = Sketch(plane)
-sketch3.box(Point2D([0,0]), Quantity(6, UNITS.cm), Quantity(6, UNITS.cm))
+sketch3.box(Point2D([0, 0]), Quantity(6, UNITS.cm), Quantity(6, UNITS.cm))
 
 # sketch3.plot()
 
 sketch4 = Sketch()
-plane = Plane(origin=Point3D([-4,0,0], UNITS.cm), direction_x=Vector3D([0,-1,0]), direction_y=Vector3D([0,0,-1]))
+plane = Plane(
+    origin=Point3D([-4, 0, 0], UNITS.cm),
+    direction_x=Vector3D([0, -1, 0]),
+    direction_y=Vector3D([0, 0, -1]),
+)
 sketch4 = Sketch(plane)
-sketch4.box(Point2D([0,0]), Quantity(3, UNITS.cm), Quantity(3, UNITS.cm))
+sketch4.box(Point2D([0, 0]), Quantity(3, UNITS.cm), Quantity(3, UNITS.cm))
 
 # sketch4.plot()
 
