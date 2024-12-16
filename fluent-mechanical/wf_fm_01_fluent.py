3213ae5d9f851af931f8f373e6dfd18d3e6bf531
dkunhamb <dipin.kunhambunair@ansys.com>
2024-12-16 10:38:50 -0600

    minor

--- fluent-mechanical/wf_fm_01_fluent.py
+++ fluent-mechanical/wf_fm_01_fluent.py
@@ -104,9 +104,9 @@ if "DOC_BUILD" in os.environ:
 
 
 import_mesh_file = examples.download_file(
-    filename="exhaust_manifold_conf.msh.h5", 
-    directory="pyansys-workflow/exhaust-manifold/pyfluent",
-    save_path=WORKING_DIR,
+    "exhaust_manifold_conf.msh.h5",
+    "pyansys-workflow/exhaust-manifold/pyfluent",
+    WORKING_DIR,
 )
 print(import_mesh_file)
 
