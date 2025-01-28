7413ee168375f22fed6c0296131be8e8c85a876b
dkunhamb <dipin.kunhambunair@ansys.com>
2025-01-28 11:49:41 -0600

    set port and instance

--- fluent-mechanical/wf_fm_02_mechanical.py
+++ fluent-mechanical/wf_fm_02_mechanical.py
@@ -98,7 +98,7 @@ if "DOC_BUILD" in os.environ:
 # Start a PyMechanical app
 # ------------------------
 #
-mechanical = launch_mechanical(batch=True, cleanup_on_exit=False)
+mechanical = launch_mechanical(batch=True, cleanup_on_exit=True)
 print(mechanical)
 
 
