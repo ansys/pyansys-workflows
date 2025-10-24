7ca910546e83b7450c90c8330034130d0fdfff54
dkunhamb <dipin.kunhambunair@ansys.com>
2025-10-24 10:46:45 -0500

    add back same workflow

--- geometry-mechanical-dpf/wf_gmd_02_mechanical.py
+++ geometry-mechanical-dpf/wf_gmd_02_mechanical.py
@@ -34,7 +34,7 @@ the heat developed in chips.
 import os
 from pathlib import Path
 
-from ansys.mechanical.core import App
+import ansys.mechanical.core as mech
 from matplotlib import image as mpimg
 from matplotlib import pyplot as plt
 
@@ -63,7 +63,8 @@ if "DOC_BUILD" in os.environ:
 # Start a PyMechanical app
 # ------------------------
 #
-app = App(globals=globals())
+app = mech.App()
+app.update_globals(globals())
 print(app)
 
 
@@ -97,36 +98,12 @@ settings_720p.CurrentGraphicsDisplay = False
 # Import geometry which is generated with pyansys-geometry
 #
 geometry_path = Path(OUTPUT_DIR, "pcb.pmdb")
-
-print("^" * 40)
-print("Importing geometry from:", geometry_path)
-print("^" * 40)
-
-print("^" * 40)
-print("Importing string geometry from:", str(geometry_path))
-print("^" * 40)
-
-try:
-    geometry_path = Path(OUTPUT_DIR, "pcb.pmdb")
-    geometry_import_group = Model.GeometryImportGroup
-    geometry_import = geometry_import_group.AddGeometryImport()
-    geometry_import_format = (
-        Ansys.Mechanical.DataModel.Enums.GeometryImportPreference.Format.Automatic
-    )
-    geometry_import_preferences = Ansys.ACT.Mechanical.Utilities.GeometryImportPreferences()
-    geometry_import_preferences.ProcessNamedSelections = True
-    geometry_import_preferences.NamedSelectionKey = ""
-    print("^" * 40)
-    print("starting import...")
-    print("^" * 40)
-
-    geometry_import.Import(str(geometry_path), geometry_import_format, geometry_import_preferences)
-    print("geometry imported successfully.")
-except Exception as e:
-    print(f"An error occurred during geometry import: with path {geometry_path}, error: {e}")
-finally:
-    app.save_as(os.path.join(OUTPUT_DIR, "geo.mechdb"), overwrite=True)
-
+geometry_import_group = Model.GeometryImportGroup
+geometry_import = geometry_import_group.AddGeometryImport()
+geometry_import_format = Ansys.Mechanical.DataModel.Enums.GeometryImportPreference.Format.Automatic
+geometry_import_preferences = Ansys.ACT.Mechanical.Utilities.GeometryImportPreferences()
+geometry_import_preferences.ProcessNamedSelections = True
+geometry_import.Import(str(geometry_path), geometry_import_format, geometry_import_preferences)
 
 # Plot geometry
 if GRAPHICS_BOOL:
@@ -138,105 +115,113 @@ if GRAPHICS_BOOL:
 # -----------------------
 #
 
-# ExtAPI.Application.ActiveUnitSystem = MechanicalUnitSystem.StandardMKS
+ExtAPI.Application.ActiveUnitSystem = MechanicalUnitSystem.StandardMKS
 
-# # Create named selection for all bodies
-# bodies = Model.Geometry.GetChildren(DataModelObjectCategory.Body, True)
-# body_ids = [bd.GetGeoBody().Id for bd in bodies]
-# selection = ExtAPI.SelectionManager.CreateSelectionInfo(SelectionTypeEnum.GeometryEntities)
-# selection.Ids = body_ids
-# ns1 = Model.AddNamedSelection()
-# ns1.Name = "all_bodies"
-# ns1.Location = selection
+# Create named selection for all bodies
+bodies = Model.Geometry.GetChildren(DataModelObjectCategory.Body, True)
+body_ids = [bd.GetGeoBody().Id for bd in bodies]
+selection = ExtAPI.SelectionManager.CreateSelectionInfo(SelectionTypeEnum.GeometryEntities)
+selection.Ids = body_ids
+ns1 = Model.AddNamedSelection()
+ns1.Name = "all_bodies"
+ns1.Location = selection
 
-# # Create named selection for all except substrate
-# substrate_id = [bd.GetGeoBody().Id for bd in bodies if bd.Name.endswith("substrate")]
-# except_substrate_id = list(set(body_ids) - set(substrate_id))
+# Create named selection for all except substrate
+substrate_id = [bd.GetGeoBody().Id for bd in bodies if bd.Name.endswith("substrate")]
+except_substrate_id = list(set(body_ids) - set(substrate_id))
 
-# selection = ExtAPI.SelectionManager.CreateSelectionInfo(SelectionTypeEnum.GeometryEntities)
-# selection.Ids = except_substrate_id
-# ns2 = Model.AddNamedSelection()
-# ns2.Name = "all_except_board"
-# ns2.Location = selection
+selection = ExtAPI.SelectionManager.CreateSelectionInfo(SelectionTypeEnum.GeometryEntities)
+selection.Ids = except_substrate_id
+ns2 = Model.AddNamedSelection()
+ns2.Name = "all_except_board"
+ns2.Location = selection
 
-# ###############################################################################
-# # Meshing
-# # -------
-# #
-# mesh = Model.Mesh
-# mesh.GenerateMesh()
+###############################################################################
+# Meshing
+# -------
+#
+mesh = Model.Mesh
+mesh.GenerateMesh()
 
-# # Export mesh image
-# ExtAPI.Graphics.Camera.SetFit()
-# ExtAPI.Graphics.ExportImage(
-#     os.path.join(OUTPUT_DIR, "mesh.png"), image_export_format, settings_720p
-# )
+# Export mesh image
+ExtAPI.Graphics.Camera.SetFit()
+ExtAPI.Graphics.ExportImage(
+    os.path.join(OUTPUT_DIR, "mesh.png"), image_export_format, settings_720p
+)
 
-# # Display the mesh
-# if GRAPHICS_BOOL:
-#     display_image("mesh.png")
+# Display the mesh
+if GRAPHICS_BOOL:
+    display_image("mesh.png")
 
 
-# ###############################################################################
-# # Analysis
-# # --------
-# # Setup steady state thermal analysis
+###############################################################################
+# Analysis
+# --------
+# Setup steady state thermal analysis
 
-# steady = Model.AddSteadyStateThermalAnalysis()
-# transient = Model.AddTransientThermalAnalysis()
+steady = Model.AddSteadyStateThermalAnalysis()
+transient = Model.AddTransientThermalAnalysis()
 
-# internal_heat_generation = steady.AddInternalHeatGeneration()
-# NSall = ExtAPI.DataModel.Project.Model.NamedSelections.GetChildren[
-#     Ansys.ACT.Automation.Mechanical.NamedSelection
-# ](True)
-# ic6 = [i for i in NSall if i.Name == "ic-6"][0]
-# internal_heat_generation.Location = ic6
-# internal_heat_generation.Magnitude.Output.SetDiscreteValue(0, Quantity(5e7, "W m^-1 m^-1 m^-1"))
+internal_heat_generation = steady.AddInternalHeatGeneration()
+NSall = ExtAPI.DataModel.Project.Model.NamedSelections.GetChildren[
+    Ansys.ACT.Automation.Mechanical.NamedSelection
+](True)
+ic6 = [i for i in NSall if i.Name == "ic-6"][0]
+internal_heat_generation.Location = ic6
+internal_heat_generation.Magnitude.Output.SetDiscreteValue(0, Quantity(5e7, "W m^-1 m^-1 m^-1"))
 
-# all_bodies = [i for i in NSall if i.Name == "all_bodies"][0]
-# convection = steady.AddConvection()
-# convection.Location = all_bodies
-# convection.FilmCoefficient.Output.DiscreteValues = [Quantity("5[W m^-2 C^-1]")]
+all_bodies = [i for i in NSall if i.Name == "all_bodies"][0]
+convection = steady.AddConvection()
+convection.Location = all_bodies
+convection.FilmCoefficient.Output.DiscreteValues = [Quantity("5[W m^-2 C^-1]")]
 
-# steady_solution = steady.Solution
-# temperature_result = steady_solution.AddTemperature()
-# steady_solution.Solve(True)
+steady_solution = steady.Solution
+temperature_result = steady_solution.AddTemperature()
+steady_solution.Solve(True)
 
-# # Transient analysis setup
-# initial_condition = transient.InitialConditions[0]
-# initial_condition.InitialTemperature = InitialTemperatureType.NonUniform
-# initial_condition.InitialEnvironment = steady
+# Transient analysis setup
+initial_condition = transient.InitialConditions[0]
+initial_condition.InitialTemperature = InitialTemperatureType.NonUniform
+initial_condition.InitialEnvironment = steady
 
-# transient_analysis_settings = transient.AnalysisSettings
-# transient_analysis_settings.StepEndTime = Quantity(200, "sec")
+transient_analysis_settings = transient.AnalysisSettings
+transient_analysis_settings.StepEndTime = Quantity(200, "sec")
 
-# internal_heat_generation2 = transient.AddInternalHeatGeneration()
+internal_heat_generation2 = transient.AddInternalHeatGeneration()
 
-# ic1 = [i for i in NSall if i.Name == "ic-1"][0]
-# internal_heat_generation2.Location = ic1
-# internal_heat_generation2.Magnitude.Output.SetDiscreteValue(0, Quantity(5e7, "W m^-1 m^-1 m^-1"))
+ic1 = [i for i in NSall if i.Name == "ic-1"][0]
+internal_heat_generation2.Location = ic1
+internal_heat_generation2.Magnitude.Output.SetDiscreteValue(0, Quantity(5e7, "W m^-1 m^-1 m^-1"))
 
-# ###############################################################################
-# # Add result objects
-# # ------------------
-# #
-# transient_solution = transient.Solution
-# transient_temperature_result = transient_solution.AddTemperature()
-# temperature_probe1 = transient_solution.AddTemperatureProbe()
-# temperature_probe1.GeometryLocation = ic6
-# temperature_probe2 = transient_solution.AddTemperatureProbe()
-# temperature_probe2.GeometryLocation = ic1
+###############################################################################
+# Add result objects
+# ------------------
+#
+transient_solution = transient.Solution
+transient_temperature_result = transient_solution.AddTemperature()
+temperature_probe1 = transient_solution.AddTemperatureProbe()
+temperature_probe1.GeometryLocation = ic6
+temperature_probe2 = transient_solution.AddTemperatureProbe()
+temperature_probe2.GeometryLocation = ic1
 
-# ###############################################################################
-# # Solve
-# # -----
-# #
-# transient_solution.Solve(True)
+###############################################################################
+# Solve
+# -----
+#
+transient_solution.Solve(True)
+
+# TODO :Remove once completed
+# Export mesh image
+Tree.Activate([transient_temperature_result])
+ExtAPI.Graphics.Camera.SetFit()
+ExtAPI.Graphics.ExportImage(
+    os.path.join(OUTPUT_DIR, "temperature.png"), image_export_format, settings_720p
+)
 
 ###############################################################################
 # Save files and close Mechanical
 # -------------------------------
 # Mechanical file (mechdb) contains results for each analysis
 #
-app.save_as(os.path.join(OUTPUT_DIR, "pcb.mechdb"), overwrite=True)
+app.save(os.path.join(OUTPUT_DIR, "pcb.mechdb"))
 app.close()
